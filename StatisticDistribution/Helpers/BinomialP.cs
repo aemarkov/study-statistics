5e7c133752034f7e794478bf760e7f6b87768763
Alex Titov <staffofmousecorp@gmail.com>
2015-11-08 20:17:24 +0300

    Реализован класс BinomicalDistribution Написан хелпер для этого (Helpers/BinomialP.cs)

---
+++ StatisticDistribution/Helpers/BinomialP.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StatisticDistribution.Helpers
+{
+    // Gmurman (c 52 - 53)
+    class BinomialP
+    {
+        static public double correct(int k, int n, double p)
+        {
+            double Cnk = (double)combinations(n, k);
+            double pk = Math.Pow(p, k);
+            double qn_k = Math.Pow(1 - p, n - k);
+            double P = Cnk * pk * qn_k;
+            return P;
+        }
+
+        static private double correct_factorial(double n)
+        {
+            double factorial = 1;
+            for (int i = 2; i <= n; ++i)
+                factorial *= i;
+            return factorial;       
+        }
+
+        // из n по k
+        static private double combinations(int n, int k)
+        {
+            return correct_factorial(n) / (correct_factorial(k) * correct_factorial(n - k));
+        }
+
+        #region UNUSED
+        static public double approximated(int k, int n, double p)
+        {
+            double lambda = n * p;
+            return Math.Pow(lambda, (double)k) * Math.Pow(Math.E, -lambda) / approximated_factorial(k);
+        }
+
+        // http://nuclphys.sinp.msu.ru/mathan/p3/m3205.html
+        // Не работает корректно
+        static private double approximated_factorial(double n)
+        {
+            return (Math.Sqrt(2.0 * Math.PI) * Math.Pow(n, (n + 0.5))) / (Math.Pow(Math.E, 2.0));
+        }
+        #endregion
+    }
+}
